8568bdb63c4c7d5dac2ed961f5e7acfe1347c25a
dimensional237e <mowale08@gmail.com>
2023-08-17 12:30:00 +0300

    mohamedw_tasks:task1: actual task

--- Tasks/mohamedw_tasks/task1.py
+++ Tasks/mohamedw_tasks/task1.py
@@ -1,1 +1,28 @@
-print("hello eyad")
+x=input("enter your name: ")
+y={'m':[[1,0,1],[1,1,1],[1,0,1]],'o':[[1,1,1],[1,0,1],[1,1,1]],'h':[[1,0,0],[1,1,1],[1,0,1]]}
+
+
+try:
+    for i in x:
+#letter 1:
+        if y[i][0] == [1,0,1]:
+            print(" * " + "  " + "  * ")
+        elif y[i][0] == [1,1,1]:
+            print(" * " + " * " + " * ")
+        elif y[i][0] == [1,0,0]:
+            print(" * " + "  " + "  ")
+#letter 2:
+        if y[i][1] == [1,1,1]:
+            print(" * " + " * " + " *  ")
+        elif y[i][1] == [1,0,1]:
+            print(" * " + "   " + " * ")
+#letter 3:
+        if y[i][2] == [1,0,1]:
+            print(" * " + "   " + " * ")
+            print("                   ")
+        elif y[i][2] == [1,1,1]:
+            print(" * " + " * " + " * ")
+            print("                   ")
+
+except:
+    print("name is soon to be added to database")
