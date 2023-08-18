117cb4f492e7f5ffdc149b07f19c62f47d6e9f5b
Eyad Elzayat <elzayateyad@gmail.com>
2023-08-18 16:32:48 +0200

    C:\Users\admin\Downloads\My-Git-hub\OverFlow_2023\Tasks\Eyad_Elzayat_tasks

---
+++ Tasks/Eyad_Elzayat_tasks/Training.py
@@ -0,0 +1,23 @@
+X = int(input("Put"))
+
+for i in range(X,0,-1):
+    print(" "*(X+1-i)+"* "*i)
+
+
+
+
+#Still working on 
+        # database = {
+        #     "E" : [[1,1,1],[1,1,0],[1,1,1]],
+        #     "Y" : [[1,0,1],[0,1,0],[0,1,0]],
+        #     "A" : [[0,1,0],[1,1,1],[1,0,1]],
+        #     "D" : [[1,1,0],[1,0,1],[1,1,0]]
+        # }
+
+        # X = input("Your name num")
+
+        # for i in X:
+        #     data = database[i]
+        #     print(data)
+
+        #     # print((database[i]))
