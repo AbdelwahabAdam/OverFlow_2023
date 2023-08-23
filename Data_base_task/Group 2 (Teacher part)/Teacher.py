0852f78c47f77d77bf3a70c20b11118e5e7c7ead
Eyad Elzayat <elzayateyad@gmail.com>
2023-08-24 01:14:30 +0200

    buld basics

--- Data_base_task/Group 2 (Teacher part)/Teacher.py
+++ Data_base_task/Group 2 (Teacher part)/Teacher.py
@@ -1,3 +1,27 @@
 # Teasting Teachers system
 
-print('hello world')
+class Teacher ():
+    def __init__(self , name,age,houre_rate,number_of_classes_given,english_teacher,math_teacher):
+        self.name = name
+        self.age = age
+        self.houre_rate = houre_rate
+        self.number_of_classes_given = number_of_classes_given
+        self.english_teacher = english_teacher
+        self.math_teacher = math_teacher    
+
+    def add(self):
+        pass
+    def list_techer(self):
+        pass
+    def filter(self):
+        pass
+    def delete(self):
+        pass
+    def update(self):
+        pass
+    def bounce (self):
+        pass
+    def classes (self):
+        pass
+    def salary (self):
+        pass
